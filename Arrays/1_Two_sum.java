61890a729a8a186853d1a33228035d58d64abfc5
manvendra singh <singhmanvendrapratap001@gmail.com>
2026-08-08 03:01:54 -0700

    Day1-Two sum Solved

---
+++ Arrays/1_Two_sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        
+        for(int i = 0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+
+                int ans = nums[i]+nums[j];
+                if(ans==target){
+                    return new int[]{i, j};
+                    
+                }
+
+            }
+
+        }
+        return new int[]{};
+        
+    }
+}
