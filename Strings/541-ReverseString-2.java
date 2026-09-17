9725bc7a88c0601217be3e704fbdcbdbd2b36039
manvendra singh <singhmanvendrapratap001@gmail.com>
2026-09-17 10:53:23 -0700

    Reverse String-2 Solution

--- Strings/541-ReverseString-2.java
+++ Strings/541-ReverseString-2.java
@@ -1,3 +1,20 @@
-public class 541=ReverseString {
-    
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+
+        for (int start = 0; start < arr.length; start += 2 * k) {
+            int i = start;
+            int j = Math.min(start + k, arr.length) - 1;
+
+            while (i < j) {
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        return new String(arr);
+    }
 }
