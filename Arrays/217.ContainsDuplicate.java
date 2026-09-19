1476c5c9c9fe0eaafb87e41e8d29900ae017205c
manvendra singh <singhmanvendrapratap001@gmail.com>
2026-09-19 23:10:08 +0530

    check contains duplicate

--- Arrays/217.ContainsDuplicate.java
+++ Arrays/217.ContainsDuplicate.java
@@ -1,3 +1,11 @@
-public class 217.ContainsDuplicate {
-    
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hashset = new HashSet<>();
+        for(int num : nums){
+            if(!hashset.add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
